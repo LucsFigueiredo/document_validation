4f56acd515929f5634f4efc1b8d8c9631d0a9a0e
LucsFigueiredo <lucasfigueiredo.s007@gmail.com>
2026-05-27 22:34:39 -0300

    server running locally

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 // Application entry point
+
+const express = require("express");
+const dotenv = require("dotenv").config({ path: "./.env" });
+const app = express();
+
+app.listen(process.env.PORT);
+console.log(`Server running on port ${process.env.PORT}`);
+
+app.use(express.static("src/public"));
