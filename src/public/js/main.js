db8fa9bac53dd9effc44af0da289a5be8760902a
LucsFigueiredo <lucasfigueiredo.s007@gmail.com>
2026-05-28 22:26:15 -0300

    rendered from `main.js` the html

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,13 @@
 // Initializate the application, render the page, add frontend logic, etc.
+
+function main() {
+  const app = document.getElementById("app");
+  app.innerHTML = `
+        <h1>Hello World!</h1>
+        <p>This is a simple Node.js application.</p>
+    `;
+}
+
+export default main;
+
+main();
